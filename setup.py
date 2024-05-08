db8910c5d5a566373d47a2cb896a2a7cfd327037
Pranai Ramineni <149972358+pranai-ramineni@users.noreply.github.com>
2024-05-08 13:56:42 -0700

    experiment finished

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='mcqgenerator',
+    version='0.0.1',
+    author='pranai raminei',
+    author_email='pr.ramineni@gmail.com',
+    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
+    packages=find_packages()
+)
+
+# pip install requirements.txt
