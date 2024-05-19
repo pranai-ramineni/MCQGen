af20b18b9f0cc7ad69b232aadef70f659145f587
Pranai Ramineni <149972358+pranai-ramineni@users.noreply.github.com>
2024-05-19 15:05:29 -0700

    update

--- src/mcqgenerator/utils.py
+++ src/mcqgenerator/utils.py
@@ -8,22 +8,17 @@ def extract_text_from_file(uploaded_file):
             file_type = uploaded_file.name.split('.')[-1].lower()
 
             if file_type == 'pdf':
-                # Open the uploaded PDF file as a binary stream
                 with io.BytesIO(uploaded_file.read()) as file:
-                    # Read the PDF file using PyPDF2
                     pdf_reader = PyPDF2.PdfReader(file)
                     
-                    # Initialize a variable to hold the extracted text
                     all_text = ""
                     
-                    # Iterate over all the pages and concatenate the text
                     for page in pdf_reader.pages:
                         all_text += page.extract_text()
                     
                     return all_text
             
             elif file_type == 'txt':
-                # Read the text file
                 text = uploaded_file.read().decode('utf-8')
                 return text
             
